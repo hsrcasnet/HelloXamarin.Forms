f4aaf40932177bf42d56725b5fc12a81862c6881
Thomas Galliker <thomas@bluewin.ch>
2019-03-13 21:36:58 +0100

    Initial commit

---
+++ HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        private int counter = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            this.counter++;
+            this.MyButton.Text = $"Clicked {this.counter}";
+        }
+    }
+}
